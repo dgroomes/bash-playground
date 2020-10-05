1c8d13e3ff8ba37b69d43a12a92bc824df8a5a69
David Groomes <dwgroomes@gmail.com>
2020-10-05 18:19:05 -0500

    Generate JSON test date JSON from awk (it's fast!)

--- misc/awk-examples.sh
+++ misc/awk-examples.sh
@@ -2,22 +2,25 @@
 # Examples using the 'awk' program
 # Referenced material:
 #  * https://www.gnu.org/software/gawk/manual/html_node/ARGC-and-ARGV.html
+#  * https://www.gnu.org/software/gawk/manual/html_node/Quoting.html
+#  * https://www.gnu.org/software/gawk/manual/html_node/For-Statement.html
+#  * https://www.gnu.org/software/gawk/manual/html_node/Concatenation.html
 
 replace() {
-  echo "hello there, %PLACEHOLDER%" | awk '{ gsub(/%PLACEHOLDER%/,"friend") }1'
+	echo "hello there, %PLACEHOLDER%" | awk '{ gsub(/%PLACEHOLDER%/,"friend") }1'
 }
 #replace
 
 # Pass variables to 'awk' with the '-v' option.
 replaceWithVariable() {
-  echo "hello there, %PLACEHOLDER%" | awk -v subject='friend' '{ gsub(/%PLACEHOLDER%/,subject) }1'
+	echo "hello there, %PLACEHOLDER%" | awk -v subject='friend' '{ gsub(/%PLACEHOLDER%/,subject) }1'
 }
 #replaceWithVariable
 
 # An alternative to 'awk' variables are 'awk' *arguments*. Arguments in awk are given as positional arguments.
 printWithArg() {
-  local message="hello there"
-  awk 'BEGIN{s=ARGV[1]; print s }' "$message"
+	local message="hello there"
+	awk 'BEGIN{s=ARGV[1]; print s }' "$message"
 }
 #printWithArg
 
@@ -28,20 +31,20 @@ printWithArg() {
 #  awk: newline in string i am on
 #  multiple lin... at source line 1
 printWithVariableMultiline() {
-  local message
-  message="i am on
+	local message
+	message="i am on
 multiple lines"
 
-  awk -v message="$message" 'BEGIN{ print v }'
+	awk -v message="$message" 'BEGIN{ print v }'
 }
 #printWithVariableMultiline
 
 # By contrast, it *is possible* to use multiline strings in awk arguments.
 printWithArgMultiline() {
-  local message
-  message="i am on
+	local message
+	message="i am on
 multiple lines"
-  awk 'BEGIN{s=ARGV[1]; ARGV[1]=""; print s }' "$message"
+	awk 'BEGIN{s=ARGV[1]; ARGV[1]=""; print s }' "$message"
 }
 #printWithArgMultiline
 
@@ -61,12 +64,12 @@ multiple lines"
 #    input record number 1, file friend
 #    source line number 1
 replaceWithArg() {
-  echo "hello there, %PLACEHOLDER%" | awk 'BEGIN{ subject=ARGV[2]; ARGV[2]=""; } { gsub(/%PLACEHOLDER%/,subject) }1' - friend
+	echo "hello there, %PLACEHOLDER%" | awk 'BEGIN{ subject=ARGV[2]; ARGV[2]=""; } { gsub(/%PLACEHOLDER%/,subject) }1' - friend
 }
 #replaceWithArg
 
 leftPad() {
-  echo $"1 22 333 4444" | awk '
+	echo $"1 22 333 4444" | awk '
   {
     $1=sprintf("%04d", $1);
     $2=sprintf("%04d", $2);
@@ -75,4 +78,40 @@ leftPad() {
     print
   }'
 }
-leftPad
+#leftPad
+
+# A no-arg invocation of awk. This is an unusual use case. It doesn't actually process any input! Why would you want to
+# use this? You might want to use awk to generate data or you would prefer to hardcode what would be the input data into
+# your awk script itself.
+noArg() {
+	awk '
+		BEGIN {
+		  sum = 1 + 2;
+			printf "I used awk to do a hardcoded arithmetic operation. The result: %d\n", sum;
+		}
+	'
+}
+#noArg
+
+# Generate a large sample of JSON-formatted data for testing purposes.
+# This is much faster than using a bash for loop.
+generateTestData() {
+	# A template string that will be used in a printf command inside awk. It is convenient to declare the template string
+	# in a bash expression because we have the convenience of using single quotes (')  that let's us express the JSON
+	# fields without an extra quoting burden. In awk, all strings must be expressed using double quotes (like most
+	# programming languages) and we can't use that trick.
+	local jsonTemplate='{ "x": %d, "y": %d }'
+	local numberOfLines=3
+	awk \
+		-v jsonTemplate="$jsonTemplate" \
+		-v numberOfLines=$numberOfLines '
+		BEGIN {
+			for (i = 1; i <= numberOfLines; i++) {
+				x = i;
+				y = numberOfLines - x;
+				printf jsonTemplate "\n", x, y;
+			}
+		}
+	'
+}
+generateTestData
