8a61cd75c2bc3c064df2dd5109a23d8354e260e4
David Groomes <dwgroomes@gmail.com>
2020-10-05 23:52:31 -0500

    generateTestData in Perl

--- perl/perl-from-bash.sh
+++ perl/perl-from-bash.sh
@@ -4,4 +4,24 @@
 replace() {
 	echo "hello from bash!" | perl -ne 's/bash/perl/; print'
 }
-replace
+#replace
+
+# Generate a large sample of JSON-formatted data for testing purposes.
+# This is much faster than using a bash for loop.
+generateTestData() {
+	perl -f <(cat <<'EOF'
+	  $json_template = '{ "x": %d, "y": %d }';
+	  $lines = 3;
+
+		for ($i = 1; $i <= $lines; $i++) {
+		  $x = $i;
+      $y = $lines - $i;
+
+      printf $json_template, $i, $y;
+      printf "\n"
+    }
+EOF
+)
+}
+generateTestData
+
