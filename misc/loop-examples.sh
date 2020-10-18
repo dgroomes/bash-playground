977b46fec84ce3a63326170e2dfee067a534ca9c
David Groomes <dwgroomes@gmail.com>
2020-10-18 16:02:12 -0500

    'loopOverOutput' example

--- misc/loop-examples.sh
+++ misc/loop-examples.sh
@@ -26,4 +26,29 @@ forI() {
 	    echo "$i"
 	done
 }
-forI
+#forI
+
+# Loop over output from another command.
+#
+# In this example, we'll find all the files in the current directory using the "ls" command.
+# Then, we'll take the output of the "ls" command and loop over it using Bash array syntax. Thankfully, this function
+# will handle file names with spaces in the name! The output of the function will be something like
+# README.md
+# args-examples.sh
+# arrays-examples.sh
+# awk-examples.sh
+# file with spaces in the name.txt
+# find-examples.sh
+# here-doc-examples.sh
+# loop-examples.sh
+# sed-examples.sh
+#
+# Notice how 'file with spaces in the name.txt' is still intact when it is printed. It is not split on its spaces! See
+# the 'arrays-examples.sh' file for more info on Bash arrays.
+loopOverOutput() {
+	files=$(ls)
+	for i in "${files[@]}"; do
+		echo "$i"
+	done
+}
+loopOverOutput
